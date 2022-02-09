a1305783d99ba3e61144de2f7ca73a9ac9908235
sypark96 <sypark96@nkia.co.kr>
2022-02-09 09:53:01 +0900

    날씨 정보 추가 및 스타일 변경

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+  Image,
+} from "react-native";
 import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "6239d2409dee0a44224b2f9e46cce1a4";
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
 
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -23,10 +33,16 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={alert}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    console.log("json", json);
+    setDays(json.daily);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
 
   return (
@@ -41,26 +57,40 @@ export default function App() {
         // indicatorStyle="white"
         showsHorizontalScrollIndicator={false}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {`${parseFloat(day.temp.day).toFixed(1)}˚`}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  style={styles.tinyLogo}
+                  source={{
+                    uri: `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`,
+                  }}
+                />
+                <Text style={styles.tinyText}>
+                  {day.weather[0].description}
+                </Text>
+              </View>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -69,16 +99,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: "black",
   },
   city: {
-    flex: 1.2,
+    flex: 1.3,
     justifyContent: "center",
     alignItems: "center",
   },
   cityName: {
     fontSize: 68,
-    fontWeight: "700",
+    fontWeight: "600",
+    color: "white",
   },
   weather: {},
   day: {
@@ -88,9 +119,19 @@ const styles = StyleSheet.create({
   temp: {
     marginTop: 50,
     fontSize: 168,
+    color: "white",
   },
   description: {
     marginTop: -30,
     fontSize: 60,
+    color: "white",
+  },
+  tinyText: {
+    fontSize: 20,
+    color: "white",
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
